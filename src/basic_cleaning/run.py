47f76abd4ef1b95d155729edc5fdc75fba7b41d5
carl <carl.schwedes@gmail.com>
2026-03-24 16:38:34 +0100

    fix data basic_cleaning bug

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,9 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
     
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Create Artifact")
     file = "clean_sample.csv"
     df.to_csv(file, index=False)
